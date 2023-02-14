124645e554814bad956b8d221cee08aacfe46d21
burcuustael <burcustael@gmail.com>
2023-02-14 13:22:13 +0300

    WebAPI işlemleri yapıldı.

---
+++ KatmanliBlogSitesi.WebAPIUsing2/Areas/Admin/Controllers/PostsController.cs
@@ -0,0 +1,116 @@
+﻿using KatmanliBlogSitesi.Entities;
+using KatmanliBlogSitesi.WebAPIUsing2.Utils;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KatmanliBlogSitesi.WebAPIUsing2.Areas.Admin.Controllers
+{
+    [Area("Admin"), Authorize(Policy = "AdminPolicy")]
+    public class PostsController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiAdres = "https://localhost:7126/api/Posts";
+
+        public PostsController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        // GET: PostsController
+        public async Task<ActionResult> Index()
+        {
+            var model = await _httpClient.GetFromJsonAsync<List<Post>>(_apiAdres);
+            return View(model);
+        }
+
+        // GET: PostsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PostsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PostsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateAsync(Post post, IFormFile? Image)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    post.Image = await FileHelper.FileLoaderAsync(Image);
+                    var response = await _httpClient.PostAsJsonAsync(_apiAdres, post);
+                   if(response.IsSuccessStatusCode)       return RedirectToAction(nameof(Index));
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Hata Oluştu!");
+                }
+            }
+
+            return View();
+        }
+
+        // GET: PostsController/Edit/5
+        public async Task<ActionResult> EditAsync(int id)
+        {
+            var model = await _httpClient.GetFromJsonAsync<Post>(_apiAdres + "/" + id);
+            return View(model);
+        }
+
+        // POST: PostsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> EditAsync(int id, Post post, IFormFile? Image)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if(Image is not null) post.Image = await FileHelper.FileLoaderAsync(Image);
+                    var cevap = await _httpClient.PutAsJsonAsync(_apiAdres + "/" + id, post);
+                    if(cevap.IsSuccessStatusCode) return RedirectToAction(nameof(Index));
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Hata Oluştu!");
+                }
+            }
+
+            return View(post);
+
+        }
+
+        // GET: PostsController/Delete/5
+        public async Task<ActionResult> DeleteAsync(int id)
+        {
+            var model = await _httpClient.GetFromJsonAsync<Post>(_apiAdres + "/" + id);
+            return View(model);
+        }
+
+        // POST: PostsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteAsync(int id, Post post)
+        {
+            try
+            {
+                var sonuc = await _httpClient.DeleteAsync(_apiAdres + "/" + id);
+                if(sonuc.IsSuccessStatusCode)      return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                ModelState.AddModelError("", "Hata Oluştu!");
+            }
+
+            return View(post);
+        }
+    }
+}
---
+++ KatmanliBlogSitesi.WebAPIUsing2/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,95 @@
+﻿using KatmanliBlogSitesi.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KatmanliBlogSitesi.WebAPIUsing2.Areas.Admin.Controllers
+{
+    [Area("Admin"), Authorize]
+    public class UsersController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiAdres = "https://localhost:7126/api/Users";
+
+        public UsersController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        // GET: UsersController
+        public async Task<ActionResult> IndexAsync()
+        {
+            var model = await _httpClient.GetFromJsonAsync<List<User>>(_apiAdres);
+            return View(model);
+        }
+
+        // GET: UsersController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UsersController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UsersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection) // resim yokken nasıl yapılmalı ???
+        {
+            try
+            {
+                return RedirectToAction(nameof(IndexAsync));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsersController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UsersController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(IndexAsync));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsersController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UsersController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(IndexAsync));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
